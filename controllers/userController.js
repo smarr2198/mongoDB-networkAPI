c5cd0db2f87dec69a3ed1dd15d1170109ea9dce5
smarr2198 <smarr2198@gmail.com>
2022-04-22 14:33:26 -0500

    setting up models

---
+++ controllers/userController.js
@@ -0,0 +1,120 @@
+const { ObjectId } = require("mongoose").Types;
+const { User, Course } = require("../models");
+
+// Aggregate function to get the number of users overall
+const headCount = async () =>
+  User.aggregate()
+    .count("userCount")
+    .then((numberOfUsers) => numberOfUsers);
+
+// Aggregate function for getting the overall grade using $avg
+const grade = async (userId) =>
+  User.aggregate([
+    // only include the given user by using $match
+    { $match: { _id: ObjectId(userId) } },
+    {
+      $unwind: "$reactions",
+    },
+    {
+      $group: {
+        _id: ObjectId(userId),
+        overallGrade: { $avg: "$reactions.score" },
+      },
+    },
+  ]);
+
+module.exports = {
+  // Get all users
+  getUsers(req, res) {
+    User.find()
+      .then(async (users) => {
+        const userObj = {
+          users,
+          headCount: await headCount(),
+        };
+        return res.json(userObj);
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // Get a single user
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .select("-__v")
+      .then(async (user) =>
+        !user
+          ? res.status(404).json({ message: "No user with that ID" })
+          : res.json({
+              user,
+              grade: await grade(req.params.userId),
+            })
+      )
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json(err);
+      });
+  },
+  // create a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((user) => res.json(user))
+      .catch((err) => res.status(500).json(err));
+  },
+  // Delete a user and remove them from the course
+  deleteUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No such user exists" })
+          : Course.findOneAndUpdate(
+              { users: req.params.userId },
+              { $pull: { users: req.params.userId } },
+              { new: true }
+            )
+      )
+      .then((course) =>
+        !course
+          ? res.status(404).json({
+              message: "User deleted, but no courses found",
+            })
+          : res.json({ message: "User successfully deleted" })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+
+  // Add an reaction to a user
+  addReaction(req, res) {
+    console.log("You are adding an reaction");
+    console.log(req.body);
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { reactions: req.body } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found with that ID :(" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // Remove reaction from a user
+  removeReaction(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $pull: { reaction: { reactionId: req.params.reactionId } } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: "No user found with that ID :(" })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+};
---
+++ models/User.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    first: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    last: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    github: {
+      type: String,
+      required: true,
+      max_length: 50,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model("user", userSchema);
+
+module.exports = User;
